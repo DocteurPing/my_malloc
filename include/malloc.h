868f57f5a54b9f05bceab4d9c2dcca6124f983ac
DrPing <cedric.ogire@hotmail.fr>
2018-01-24 13:48:25 +0100

    Malloc: first push doesn't work jsut waiting for debbuging

--- include/malloc.h
+++ include/malloc.h
@@ -8,6 +8,11 @@
 #ifndef MALLOC_H_
 	#define MALLOC_H_
 
+#include <unistd.h>
+#include <stdio.h>
+
+void *base = NULL;
+
 enum e_bool {
 	FALSE,
 	TRUE
@@ -15,7 +20,7 @@ enum e_bool {
 
 typedef struct	s_header_malloc {
 	struct s_header_malloc *next;
-	void *contain;
+	void *current;
 	unsigned int size;
 	enum e_bool is_free;
 } t_header_malloc;
--- src/malloc.c
+++ src/malloc.c
@@ -5,3 +5,58 @@
 ** malloc.c
 */
 
+#include "../include/malloc.h"
+
+void *get_base()
+{
+	return (sbrk(0));
+}
+
+void *alloc_end(size_t size) 
+{
+	void *ptr = sbrk(0);
+	void *ptr_next = sbrk(size);
+	t_header_malloc *header = ptr;
+	
+	(void)ptr_next;
+	header->next = NULL;
+	header->size = size - sizeof(t_header_malloc);
+	header->current = header + sizeof(t_header_malloc);
+	header->is_free = FALSE;
+	return (header->current);
+}
+
+void *find_best_feed(void *base, size_t size)
+{
+	t_header_malloc *header = base;
+
+	while (header->next) {
+		if (header->size > size && header->is_free)
+			return (header->current);
+		header = header->next;
+	}
+	return (alloc_end(size));
+}
+
+void *malloc(size_t size)
+{
+	void *ptr_return;
+
+	printf("Malloc: starting\n");
+	if (!base) {
+		base = get_base();
+		ptr_return = alloc_end(size + sizeof(t_header_malloc));
+		base = ptr_return;
+		return (ptr_return);
+	}
+	return (find_best_feed(base, size + sizeof(t_header_malloc)));
+}
+
+void free(void *ptr)
+{
+	t_header_malloc *header = base;
+
+	while (header->current != ptr)
+		header = header->next;
+	header->is_free = TRUE;
+}
